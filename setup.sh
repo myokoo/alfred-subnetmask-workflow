76dc2e42a4061720c865dc1effe00a6458d3be19
myokoo <mizuki@myokoo.jp>
2020-10-15 20:54:22 +0900

    setup.sh corresponds to v4.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-LIB="$HOME/Library/Application Support/Alfred 3/Alfred.alfredpreferences/workflows"
+LIB1="$HOME/Library/Application Support/Alfred 3/Alfred.alfredpreferences"
+LIB2="$HOME/Library/Application Support/Alfred/Alfred.alfredpreferences"
+
+[[ -d "$LIB1" ]] && LIB="$LIB1/workflows"
+[[ -d "$LIB2" ]] && LIB="$LIB2/workflows"
+
+[[ ! -d "$LIB" ]] && mkdir $LIB
 cd "$LIB" || exit 2
 
-ln -fs "$SCRIPT_DIR" SubnetMask && echo "setup ok," || echo error
+ln -fs "$SCRIPT_DIR" "$LIB/SubnetMask" && echo "setup ok,"
